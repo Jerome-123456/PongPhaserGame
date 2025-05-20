15d6eccdf90751a4f448b029a7482bed05d2ff45
Jerome-123456 <DAmbrose25@synergymat.uk>
2025-05-20 12:50:37 +0100

    update

--- Client/game.js
+++ Client/game.js
@@ -23,17 +23,21 @@ const game = new Phaser.Game(config);
 function preload() {}
 
 function create() {
+    wasd = this.input.keyboard.addKeys({
+    up: Phaser.Input.Keyboard.KeyCodes.W,
+    down: Phaser.Input.Keyboard.KeyCodes.S
+    });
     ball = this.physics.add.sprite(400, 300, null).setDisplaySize(15, 15).setTint(0xffffff);
  
     // Add rectangle paddles
-    paddleLeft = this.add.rectangle(500, 300, 200, 100, 0xffffff);
-    paddleRight = this.add.rectangle(750, 300, 200, 100, 0xffffff);
+    paddleLeft = this.add.rectangle(50, 300, 20, 100, 0xffffff);
+    paddleRight = this.add.rectangle(750, 300, 20, 100, 0xffffff);
  
     // Enable physics
     this.physics.add.existing(paddleLeft, true);
     this.physics.add.existing(paddleRight, true);
  
-    ball.setVelocity(200, 200);
+    ball.setVelocity(250, 250);
     ball.setCollideWorldBounds(true);
     ball.setBounce(1);
  
@@ -44,11 +48,17 @@ function create() {
 }
 
 function update() {
-    if (cursors.up.isDown) paddleLeft.y -= 5;
-    else if (cursors.down.isDown) paddleLeft.y += 5;
-
+    // Right paddle (Player 1)
+    if (cursors.up.isDown) paddleRight.y -= 5;
+    else if (cursors.down.isDown) paddleRight.y += 5;
+ 
+    // Left paddle (Player 2)
+    if (wasd.up.isDown) paddleLeft.y -= 5;
+    else if (wasd.down.isDown) paddleLeft.y += 5;
+ 
     paddleLeft.body.updateFromGameObject();
-
+    paddleRight.body.updateFromGameObject();
+ 
     if (ball.x < 0 || ball.x > 800) {
         fetch('https://localhost:5001/api/scores', {
             method: 'POST',
